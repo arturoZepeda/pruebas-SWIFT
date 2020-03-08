bb38145c45f31e3f579368b628a5c9c622545d56
AZEPEDA <aaze92@gmail.com>
2020-03-08 17:32:42 -0600

    Initial Commit

---
+++ PRUEBASINICIALES/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PRUEBASINICIALES
+//
+//  Created by DANK on 08/03/20.
+//  Copyright © 2020 DANK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
